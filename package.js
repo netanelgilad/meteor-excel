97a7696804d548fc8a11d8f7fa9972a23f7c25c7
Netanel Gilad <netanelgilad@gmail.com>
2015-04-24 17:09:57 +0300

    bump 0.2.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'netanelgilad:excel',
   summary: 'Parse excel worksheets for your meteor app.',
-  version: '0.1.1',
+  version: '0.2.0',
   git: 'https://github.com/netanelgilad/meteor-excel'
 });
 
